f57cac2d4ed985cb99073d23dacb8b0f1f7daeec
Santi871 <santivegega@gmail.com>
2016-06-24 00:46:39 -0300

    added summary command

--- reddit_bot/commands_handler.py
+++ reddit_bot/commands_handler.py
@@ -2,6 +2,13 @@ import praw
 import OAuth2Util
 import requests
 import json
+import datetime
+import numpy as np
+import math
+import os
+from tokens import tokens
+from imgurpython import ImgurClient
+import matplotlib.pyplot as plt
 from time import sleep
 from reddit_bot import bot_threading
 
@@ -10,6 +17,7 @@ class CommandsHandler:
 
     def __init__(self):
         self.r = praw.Reddit(user_agent="windows:RedditSlacker 0.1 by /u/santi871")
+        self.imgur = ImgurClient(tokens.get_token('IMGUR_CLIENT_ID'), tokens.get_token('IMGUR_CLIENT_SECRET'))
         self._authenticate()
 
     def _authenticate(self):
@@ -23,19 +31,190 @@ class CommandsHandler:
                                             self.handle_command_request, request)
         thread.start()
 
-    def handle_command_request(self, response_url):
+    def handle_command_request(self, request):
 
-        payload = {
-            "text": "This is a line of text.\nAnd this is another one."
-        }
+        response_url = request.get('response_url')
+        command = request.get('command')[1:]
 
-        sleep(5)
+        payload = getattr(self, command)(request.get('text').split())
+        print(str(payload))
 
-        headers = {'content-type': 'application/json'}
-
-        response = requests.post(response_url, data=json.dumps(payload), headers=headers)
+        response = requests.post(response_url, data=json.dumps(payload), headers={'content-type': 'application/json'})
 
         return response
+
+    def summary(self, args):
+
+        i = 0
+        total_comments = 0
+        subreddit_names = []
+        subreddit_total = []
+        ordered_subreddit_names = []
+        comments_in_subreddit = []
+        ordered_comments_in_subreddit = []
+        comment_lengths = []
+        history = {}
+        total_karma = 0
+        troll_index = 0
+        troll_likelihood = "Low"
+        blacklisted_subreddits = ('theredpill', 'rage', 'atheism', 'conspiracy', 'subredditdrama', 'subredditcancer',
+                                  'SRSsucks', 'drama', 'undelete', 'blackout2015', 'oppression0', 'kotakuinaction',
+                                  'tumblrinaction', 'offensivespeech', 'bixnood')
+        total_negative_karma = 0
+        limit = 500
+        user = self.r.get_redditor(args[0])
+        x = []
+        y = []
+        s = []
+
+        karma_accumulator = 0
+        karma_accumulated = []
+        karma_accumulated_total = []
+
+        for comment in user.get_comments(limit=limit):
+
+            displayname = comment.subreddit.display_name
+
+            if displayname not in subreddit_names:
+                subreddit_names.append(displayname)
+
+            subreddit_total.append(displayname)
+
+            total_karma = total_karma + comment.score
+
+            x.append(datetime.datetime.utcfromtimestamp(float(comment.created_utc)))
+            y.append(comment.score)
+            comment_lengths.append(len(comment.body.split()))
+
+            if comment.score < 0:
+                total_negative_karma += comment.score
+
+            if len(comment.body) < 200:
+                troll_index += 0.1
+
+            if displayname in blacklisted_subreddits:
+                troll_index += 2.5
+
+            i += 1
+
+        total_comments_read = i
+
+        troll_index *= limit / total_comments_read
+
+        average_karma = np.mean(y)
+
+        if average_karma >= 5 and total_negative_karma > (-70 * (total_comments_read / limit)) and troll_index < 50:
+            troll_likelihood = 'Low'
+
+        if troll_index >= 40 or total_negative_karma < (-70 * (total_comments_read / limit)) or average_karma < 1:
+            troll_likelihood = 'Moderate'
+
+        if troll_index >= 60 or total_negative_karma < (-130 * (total_comments_read / limit)) or average_karma < -2:
+            troll_likelihood = 'High'
+
+        if troll_index >= 80 or total_negative_karma < (-180 * (total_comments_read / limit)) or average_karma < -5:
+            troll_likelihood = 'Very high'
+
+        if troll_index >= 100 or total_negative_karma < (-200 * (total_comments_read / limit)) or average_karma < -10:
+            troll_likelihood = 'Extremely high'
+
+        print(troll_index)
+        print(total_negative_karma)
+
+        for subreddit in subreddit_names:
+            i = subreddit_total.count(subreddit)
+            comments_in_subreddit.append(i)
+            total_comments += i
+
+        i = 0
+
+        for subreddit in subreddit_names:
+
+            if comments_in_subreddit[i] > (total_comments_read / (20 * (limit / 200)) / (len(subreddit_names) / 30)):
+                history[subreddit] = comments_in_subreddit[i]
+
+            i += 1
+
+        old_range = 700 - 50
+        new_range = 2000 - 50
+
+        for item in comment_lengths:
+            n = (((item - 50) * new_range) / old_range) + 50
+            s.append(n)
+
+        history_tuples = sorted(history.items(), key=lambda x: x[1])
+
+        for each_tuple in history_tuples:
+            ordered_subreddit_names.append(each_tuple[0])
+            ordered_comments_in_subreddit.append(each_tuple[1])
+
+        user_karma_atstart = user.comment_karma - math.fabs((np.mean(y) * total_comments_read))
+
+        for item in list(reversed(y)):
+            karma_accumulator += item
+            karma_accumulated.append(karma_accumulator)
+
+        for item in karma_accumulated:
+            karma_accumulated_total.append(user_karma_atstart + item)
+
+        plt.style.use('ggplot')
+        labels = ordered_subreddit_names
+        sizes = ordered_comments_in_subreddit
+        colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'teal', 'chocolate', 'olivedrab', 'tan']
+        plt.subplot(3, 1, 1)
+        plt.rcParams['font.size'] = 8
+        plt.pie(sizes, labels=labels, colors=colors,
+                autopct=None, startangle=90)
+        plt.axis('equal')
+        plt.title('User summary for /u/' + args[0], loc='center', y=1.2)
+
+        ax1 = plt.subplot(3, 1, 2)
+        x_inv = list(reversed(x))
+        plt.rcParams['font.size'] = 10
+        plt.scatter(x, y, c=y, vmin=-50, vmax=50, s=s, cmap='RdYlGn')
+        ax1.set_xlim(x_inv[0], x_inv[total_comments_read - 1])
+        ax1.axhline(y=average_karma, xmin=0, xmax=1, c="lightskyblue", linewidth=2, zorder=4)
+        plt.ylabel('Karma of comment')
+
+        ax2 = plt.subplot(3, 1, 3)
+        plt.plot_date(x, list(reversed(karma_accumulated_total)), '-r')
+        plt.xlabel('Comment date')
+        plt.ylabel('Total comment karma')
+
+        filename = args[0] + "_summary.png"
+
+        figure = plt.gcf()
+        figure.set_size_inches(11, 12)
+
+        plt.savefig(filename)
+
+        path = os.getcwd() + "\\" + filename
+
+        link = self.imgur.upload_from_path(path, config=None, anon=True)
+        os.remove(path)
+
+        plt.clf()
+
+        response = {}
+        response['response_type'] = "in_channel"
+        response['attachments'] = [{}]
+        response['attachments'][0]['fields'] = [{}, {}]
+        response['attachments'][0]['fields'][0]['title'] = 'Troll likelihood'
+        response['attachments'][0]['fields'][0]['value'] = troll_likelihood
+        response['attachments'][0]['fields'][0]['short'] = True
+        response['attachments'][0]['fields'][1]['title'] = 'Total comments read'
+        response['attachments'][0]['fields'][1]['value'] = total_comments_read
+        response['attachments'][0]['fields'][1]['short'] = True
+        response['attachments'][0]['fallback'] = "Summary for /u/" + args[0]
+        response['attachments'][0]['title'] = "Summary for /u/" + args[0]
+        response['attachments'][0]['image_url'] = link['link']
+        response['attachments'][0]['color'] = "#764FA5"
+        print(str(response))
+
+        return response
+
+
+
 
 
 
--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response
 from reddit_bot import commands_handler
 from tokens import tokens
+import json
 
 SLACK_SLASHCMDS_SECRET = tokens.get_token("SLACK_SLASHCMDS_SECRET")
 commands_handler_obj = commands_handler.CommandsHandler()
@@ -9,17 +10,9 @@ app = Flask(__name__)
 
 @app.route('/slackcommands', methods=['POST'])
 def command():
-    print(str(request.form))
     if request.form.get('token') == SLACK_SLASHCMDS_SECRET:
-
-        response_url = request.form.get('response_url')
-
-        response = commands_handler_obj.thread_command_request(response_url)
+        response = commands_handler_obj.thread_command_request(request.form)
         print(response)
-        # response =
-
-        # return Response(response=json.dumps(response), status=200, mimetype='application/json')
-
         return "Processing your request..."
 
     else:
