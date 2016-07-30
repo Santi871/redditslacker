f41d80a74e2531ec16f21c884829c199e28d8415
Santi871 <santivegega@gmail.com>
2016-07-29 21:35:14 -0300

    added keyboard interrupt catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from webapp.webapp import app
-from sys import exit
+import sys
 
 if __name__ == '__main__':
     context = ('santihub.crt', 'santihub.key')
@@ -7,5 +7,5 @@ if __name__ == '__main__':
         app.run(host='0.0.0.0', ssl_context=context, threaded=True)
     except KeyboardInterrupt:
         print("Bye")
-        exit()
+        sys.exit()
 
